d0ccb49d07fe2a71fd169dae19ffc71443bdbb21
Giorgio Caculli <giorgiocaculli@gmail.com>
2021-03-03 11:54:07 +0100

    Activation Menu

--- src/be/helha/ttmc/Main.java
+++ src/be/helha/ttmc/Main.java
@@ -11,7 +11,7 @@ public class Main
     public static void main( String args[] )
     {
         System.out.println( "TTMC by Giorgio Caculli LA196672, Guillaume Lambert LA198116, Tanguy Taminiau LA199566" );
-   //     new Menu( args );
+        new Menu( args );
         Application.launch(MainGui.class, args);
     }
 }
