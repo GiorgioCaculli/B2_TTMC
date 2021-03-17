829531dc5ef9eb7f5b5711b3b7413034843ea702
GIORGIO CACULLI LA196672 <la196672@student.helha.be>
2021-03-17 16:44:10 +0100

    Better PAW image & mp3s to wavs

--- pGroupeB05/src/be/helha/ttmc/ui/gui/MainPaneSP.java
+++ pGroupeB05/src/be/helha/ttmc/ui/gui/MainPaneSP.java
@@ -21,7 +21,7 @@ public class MainPaneSP extends StackPane
         getChildren().get( 0 ).setVisible( true );
         try
         {
-            musics = new MusicGestion( "assets/musics/CreativeDestruction.wav" );
+            musics = new MusicGestion( "assets/musics/EVAmusic.wav" );
             musics.start();
         }
         catch ( Exception e )
