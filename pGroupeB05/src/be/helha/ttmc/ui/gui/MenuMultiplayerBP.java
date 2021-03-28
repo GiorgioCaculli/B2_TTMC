b17853a7be213f6be0b9e44631a41227f2eeb16e
GIORGIO CACULLI LA196672 <la196672@student.helha.be>
2021-03-28 20:25:25 +0200

    Init multiplayer local cleaner

--- pGroupeB05/src/be/helha/ttmc/ui/gui/MenuMultiplayerBP.java
+++ pGroupeB05/src/be/helha/ttmc/ui/gui/MenuMultiplayerBP.java
@@ -1,8 +1,5 @@
 package be.helha.ttmc.ui.gui;
 
-import java.util.Optional;
-import java.util.Random;
-
 import be.helha.ttmc.model.Deck;
 import be.helha.ttmc.ui.gui.MenuPlayBP.MenuPlayMainVB;
 import javafx.event.ActionEvent;
@@ -11,7 +8,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -92,36 +88,6 @@ public class MenuMultiplayerBP extends BorderPane
                         }
                     }
                     JouerChoixQuestionMultiplayerBP jcqm = new JouerChoixQuestionMultiplayerBP( d );
-                    jcqm.setScorePlayer2( 0 );
-                    TextInputDialog userNameDialogPlayer1 = new TextInputDialog();
-                    userNameDialogPlayer1.setTitle( "Insert your nickname" );
-                    userNameDialogPlayer1.setHeaderText( "Insert your nickname" );
-                    userNameDialogPlayer1.setContentText( "Please, insert your nickname:" );
-                    Optional< String > userNamePlayer1 = userNameDialogPlayer1.showAndWait();
-                    TextInputDialog userNameDialogPlayer2 = new TextInputDialog();
-                    userNameDialogPlayer2.setTitle( "Insert your nickname" );
-                    userNameDialogPlayer2.setHeaderText( "Insert your nickname" );
-                    userNameDialogPlayer2.setContentText( "Please, insert your nickname:" );
-                    Optional< String > userNamePlayer2 = userNameDialogPlayer2.showAndWait();
-                    if( userNamePlayer1.get().isEmpty() )
-                    {
-                        Random randId1 = new Random(999);
-                        jcqm.setNickNamePlayer1( String.format( "User-%d", randId1.nextInt() ) );
-                    }
-                    else
-                    {
-                        jcqm.setNickNamePlayer1( String.format( "%s", userNamePlayer1.get() ) );
-                    }
-                    if( userNamePlayer2.get().isEmpty() )
-                    {
-                        Random randId2 = new Random(999);
-                        jcqm.setNickNamePlayer2( String.format( "User-%d", randId2.nextInt() ) );
-                    }
-                    else
-                    {
-                        jcqm.setNickNamePlayer2( String.format( "%s", userNamePlayer2.get() ) );
-                    }
-                    jcqm.getLblScore().setText( String.format( ": %s - %s:", jcqm.getNickNamePlayer1(), jcqm.getNickNamePlayer2() ) );
                     getChoicePane().getChildren().add( jcqm );
                     setVisibleNode( jcqm.getClass().getSimpleName() );
                 }
