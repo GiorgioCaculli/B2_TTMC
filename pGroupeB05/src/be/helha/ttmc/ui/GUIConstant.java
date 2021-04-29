3ae1a6ad0f088d0fef58513d3321a7758ed5b388
GIORGIO CACULLI LA196672 <la196672@student.helha.be>
2021-04-29 17:44:55 +0200

    gitignore application.properties test

--- pGroupeB05/src/be/helha/ttmc/ui/GUIConstant.java
+++ pGroupeB05/src/be/helha/ttmc/ui/GUIConstant.java
@@ -9,15 +9,19 @@ public class GUIConstant
             settings.getLocale() );
     public static final String TITLE = rb.getString( "title" );
     
-    // BUTTONS
+    // BUTTONS MAIN MENU
     public static final String BUTTON_PLAY = rb.getString( "button_play" );
     public static final String BUTTON_SETTINGS = rb.getString( "button_settings" );
     public static final String BUTTON_LEAVE_GAME = rb.getString( "button_leave_game" );
     public static final String BUTTON_ADMIN_PANEL = rb.getString( "button_admin_panel" );
     public static final String BUTTON_CREDITS = rb.getString( "button_credits" );
+    
+    // BUTTONS MENU PAUSE
     public static final String BUTTON_RESUME = rb.getString( "button_resume" );
     public static final String BUTTON_BACK_TO_MAIN_MENU = rb.getString( "button_back_to_main_menu" );
     public static final String BUTTON_BACK_TO_LOBBY = rb.getString( "button_back_to_lobby" );
+    
+    // BUTTONS GENERIC
     public static final String BUTTON_RETURN = rb.getString( "button_return" );
     
     // DIALOG exit
