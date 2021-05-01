ea541756141ce8ef775375502deb7f1a4ed6f1ce
Guillaume Lambert <onyxkira2@gmail.com>
2021-05-02 00:09:06 +0200

    reduction de la police (taille poulet)

--- pGroupeB05/src/be/helha/ttmc/ui/GUIConstant.java
+++ pGroupeB05/src/be/helha/ttmc/ui/GUIConstant.java
@@ -22,6 +22,6 @@ public class GUIConstant
     { new Stop( 0, Color.BLUEVIOLET ), new Stop( 0.3, Color.ROYALBLUE ), new Stop( 0.7, Color.LIGHTSTEELBLUE ) };
     public static final LinearGradient BUTTON_GRADIENT = new LinearGradient( 0, 1, 0, 0, true, CycleMethod.NO_CYCLE,
             BUTTON_ETAPES );
-    public static final Font BUTTON_TEXT = Font.font( "Times New Roman", FontWeight.BOLD, FontPosture.ITALIC, 75 );
+    public static final Font BUTTON_TEXT = Font.font( "Times New Roman", FontWeight.BOLD, FontPosture.ITALIC, 65 );
     public static final Effect BUTTON_EFFECT = new DropShadow( 25, 13, 13, Color.DARKSLATEGREY );
 }
